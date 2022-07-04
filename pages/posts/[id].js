24b71756d22d0defca97d150d0f381beeb27d859
AHMET ARAPOĞLU <45310027+ahmetarapoglu@users.noreply.github.com>
2022-07-04 18:48:16 +0300

    Update [id].js

--- pages/posts/[id].js
+++ pages/posts/[id].js
@@ -11,7 +11,7 @@ function Post(props) {
 export default Post;
 
 export const getServerSideProps = async (context) => {
-    const res = await fetch("/api/posts/"+context.params.id+"",{
+    const res = await fetch("http://localhost:3000/api/posts/"+context.params.id+"",{
       method: 'GET',
       headers: {
         'Authorization': 'Bearer api-token',
