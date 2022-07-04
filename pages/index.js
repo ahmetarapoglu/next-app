e870e620da7751574b38a794a7eda84f178fcfa1
AHMET ARAPOĞLU <ah.ihab.arab3@gmail.com>
2022-07-04 18:41:59 +0300

    first comment
    no thing to add

--- pages/index.js
+++ pages/index.js
@@ -27,7 +27,7 @@ export default function Home(props) {
 }
 
 export const getServerSideProps = async () => {
-  const res = await fetch("/api/posts",{
+  const res = await fetch("api/posts",{
     method: 'GET',
     headers: {
       'Authorization': 'Bearer api-token',
